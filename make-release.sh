7e051bdcdb6d75f4e99ae3b55ecb9d66ef85d4e7
Fernando Stoiano Gonzalez <fernando.gonzalez@brasilct.com>
2017-04-05 17:12:49 -0300

    Update script

--- make-release.sh
+++ make-release.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 mvn clean
-mvn release:clean release:prepare
+mvn --batch-mode -Dtag=v0.0.5 -Dproject.rel.br.com.test.My-Tests=0.0.5 -Dproject.dev.br.com.test.My-Tests=0.0.6-SNAPSHOT release:clean release:prepare
 git pull origin master
 git push origin master
