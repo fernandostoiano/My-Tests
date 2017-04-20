51decf6c48775e4abceb86725567030117fde404
Fernando Stoiano Gonzalez <fernando.gonzalez@brasilct.com>
2017-04-20 16:04:45 -0300

    Alteracao referente ao jfrog

--- make-release.sh
+++ make-release.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 mvn clean
-mvn --batch-mode -Dtag=v0.0.6 -Dproject.rel.br.com.test.My-Tests=0.0.6 -Dproject.dev.br.com.test.My-Tests=0.0.7-SNAPSHOT release:clean release:prepare
+mvn --batch-mode -Dtag=v0.0.7 -Dproject.rel.br.com.test.My-Tests=0.0.7 -Dproject.dev.br.com.test.My-Tests=0.0.8-SNAPSHOT release:clean release:prepare
 git pull origin master
 git push origin master
